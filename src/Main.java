490f0788e2302a48b39869c273fd34528eda2f30
Rory <G00422319@atu.ie>
2023-11-13 10:03:49 +0000

    written code that can handle an exeption

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Please enter a number between 0 - 9");
+        Scanner myscan = new Scanner(System.in);
+        int x = 1;
+        while (x == 0) {
+
+            try {
+                //read user input as a string
+                String input = myscan.nextLine();
+
+                //Pass the input to an integer
+                int myNum = Integer.parseInt(input);
+
+                if (myNum > 0 && myNum <= 9) {
+                    System.out.println("You entered" + myNum);
+                    x++;
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a numeric value");
+
+            }
+        }
     }
 }
